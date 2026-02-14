d4133829047ea6fcd997a4f5c4e38cd004d2459f
minhdoan <doankhacminh2301@gmail.com>
2026-02-14 17:41:29 +0700

    add: top bar layout of Home Screen

--- app/src/main/java/com/example/musike/myapp/ui/view/home/HomeActivity.kt
+++ app/src/main/java/com/example/musike/myapp/ui/view/home/HomeActivity.kt
@@ -1,28 +1,38 @@
 package com.example.musike.myapp.ui.view.home
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.musike.myapp.ui.theme.MusikeTheme
+import com.example.musike.myapp.ui.view.home.component.ScreenHomeTop
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MusikeTheme {
-
+                Column(modifier = Modifier.fillMaxSize()) {
+                    ScreenHomeTop(
+                        12, 30,
+                        onClickNotification = {
+                            Toast.makeText(this@HomeActivity, "notification", Toast.LENGTH_SHORT)
+                                .show()
+                        }, onClickAccount = {
+                            Toast.makeText(this@HomeActivity, "account", Toast.LENGTH_SHORT)
+                                .show()
+                        })
+                }
             }
         }
     }
+
 }
--- app/src/main/java/com/example/musike/myapp/ui/view/home/component/ScreenHomeTop.kt
+++ app/src/main/java/com/example/musike/myapp/ui/view/home/component/ScreenHomeTop.kt
@@ -1,2 +1,91 @@
 package com.example.musike.myapp.ui.view.home.component
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.musike.R
+
+@Composable
+fun ScreenHomeTop(
+    paddingHorizontal: Int,
+    paddingVertical: Int,
+    onClickNotification: () -> Unit,
+    onClickAccount: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(horizontal = paddingHorizontal.dp, vertical = paddingVertical.dp)
+    ) {
+
+        Text(
+            text = "Good morning Minh !",
+            fontSize = 21.sp,
+            color = colorResource(R.color.black),
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Start,
+            modifier = Modifier.weight(1f)
+        )
+
+        ButtonIcon(
+            iconRes = R.drawable.ic_home_notification,
+            pStart = 10,
+            pTop = 10,
+            pEnd = 7,
+            pBottom = 10,
+            onClick = onClickNotification
+        )
+
+        ButtonIcon(
+            iconRes = R.drawable.ic_home_account,
+            pStart = 7,
+            pTop = 10,
+            pEnd = 10,
+            pBottom = 10,
+            onClick = onClickAccount
+        )
+    }
+}
+
+@Composable
+private fun ButtonIcon(
+    modifier: Modifier = Modifier,
+    iconRes: Int,
+    pStart: Int, pTop: Int, pEnd: Int, pBottom: Int,
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .wrapContentSize(align = Alignment.Center)
+            .scale(1.2f)
+            .clickable { onClick() }
+            .padding(start = pStart.dp, top = pTop.dp, end = pEnd.dp, bottom = pBottom.dp)
+    ) {
+        Icon(
+            painter = painterResource(iconRes),
+            null,
+            modifier = Modifier.wrapContentSize(),
+        )
+    }
+}
